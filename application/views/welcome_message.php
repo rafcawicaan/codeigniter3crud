21591f556e75731aff73f79258f90996aa309ef6
Rafael Cawicaan <rafcawicaan@gmail.com>
2024-12-30 13:44:38 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>CodeIgniter</title>
+	<title></title>
 
 	<style type="text/css">
 
